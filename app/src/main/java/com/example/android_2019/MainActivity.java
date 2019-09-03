c5edc68da97e900aa5c3223a44cea26b2b211d9f
MY-CS <matsune@ubi.cs.ritsumei.ac.jp>
2019-09-03 19:08:09 +0900

    third commit / add walking

--- app/src/main/java/com/example/android_2019/MainActivity.java
+++ app/src/main/java/com/example/android_2019/MainActivity.java
@@ -14,11 +14,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.learn).setOnClickListener(this);
+        findViewById(R.id.learn_walking).setOnClickListener(this);
     }
 
     //ボタンが押された時の処理
     public void onClick(View view){
-        Intent intent = new Intent(this, learn.class);  //インテントの作成
-        startActivity(intent);                                 //画面遷移
+        switch(view.getId()){
+            case R.id.learn:
+                Intent intent = new Intent(this, learn.class);  //インテントの作成
+                startActivity(intent);
+                break;
+            case R.id.learn_walking:
+                Intent intent2 = new Intent(this, learn_walking.class);  //インテントの作成
+                startActivity(intent2);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/android_2019/learn_walking.java
@@ -0,0 +1,137 @@
+package com.example.android_2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+//setContentView(R.layout.activity_learn);
+
+public class learn_walking extends AppCompatActivity implements Runnable, SensorEventListener{
+
+    SensorManager sm;
+    TextView tv;
+    Handler h;
+    float gx, gy, gz;
+    double acc;
+    String file_name = "data_walking.csv";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout ll = new LinearLayout(this);
+        setContentView(ll);
+
+        tv = new TextView(this);
+        ll.addView(tv);
+
+        h = new Handler();
+        h.postDelayed(this, 500);
+    }
+
+    @Override
+    public void run() {
+        acc = Math.sqrt(gx * gx + gy * gy + gz * gz);
+        tv.setText("X-axis : " + gx + "\n"
+                + "Y-axis : " + gy + "\n"
+                + "Z-axis : " + gz + "\n"
+                + "ACCis : " + acc + "\n");
+
+        //ここからファイルの書き込みの追加
+//        acc = Math.sqrt(gx * gx + gy * gy + gz * gz);
+        saveFile(file_name, acc);
+        //
+
+        h.postDelayed(this, 500);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if (0 < sensors.size()) {
+            sm.registerListener(this, sensors.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        h.removeCallbacks(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        gx = event.values[0];
+        gy = event.values[1];
+        gz = event.values[2];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    //csvファイルの書き込み
+    //csvというか一応文字列の数値をカンマ区切りで書き込む感じにとりあえずした
+    //ファイルの中身を見てみた感じ, いけてるとは思う
+    //x軸だけでやってみるとちゃんと変化して書き込まれたけど, 合成加速度にすると全然変化がしないんだけど
+    // ACCも表示してみた感じ変化してはいるが, 全然変化しないのはなんでなんだろ
+    public void saveFile(String file_name, double data) {
+
+        // try-with-resources
+        try {
+            FileOutputStream fileOutputstream = openFileOutput(file_name, MODE_APPEND);
+            fileOutputstream.write("0".getBytes());
+            fileOutputstream.write(",".getBytes());
+            fileOutputstream.write(String.valueOf(data).getBytes());
+            fileOutputstream.write("\n".getBytes());
+
+            // 出力ファイルの作成
+            // 上の奴を使わないとfilesにファイルができない
+//            FileWriter f = new FileWriter(file_name, true);
+//            PrintWriter p = new PrintWriter(new BufferedWriter(f));
+//
+//            // 書き込み
+//            p.print(0); // クラスラベルのつもり
+//            p.print(",");
+//            p.print(data); // 合成加速度
+//            p.println();   // 改行
+//
+//            // ファイルに書き出し閉じる
+//            p.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
